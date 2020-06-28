b33d0465644cef4cac321ddafec8b74faa3f3e4c
Moises Ferreira Da Silva <51299210+FMOISES@users.noreply.github.com>
2020-06-28 10:53:29 -0300

    Create multiplication_table.php

---
+++ multiplication_table.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+<?php
+/*
+        while ($n1 < 10){
+        echo $n1.'X'. $n2 .'=' .($n1*$n2);
+        echo'</br>';
+        $n1++;
+     }  */
+     for($n2 = 1;$n2 <= 10;$n2++){
+        for($n1 = 1;$n1 <= 10;$n1++){
+        echo $n1.'X'. $n2 .'=' .($n1*$n2);
+        echo'</br>';
+     }
+     echo'</br>';
+     echo'</br>';
+     }
+
+?>
+</body>
+</html>
